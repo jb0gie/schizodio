0c5096b7fd6acf5a5c7da24e0666286a2d07c2df
bigjax <60930630+jaxnalia@users.noreply.github.com>
2025-08-05 15:12:43 +0100

    Move audio to layout and update AudioPlayer

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+@@ .. @@
+ <script lang="ts">
+   import router from 'svelte-spa-router';
+   import { currentTheme } from './lib/stores/theme';
++  import { initializeAudio } from './lib/stores/audio';
++  import { onMount } from 'svelte';
+   import Home from './lib/components/Home.svelte';
+   import Mint from './lib/Mint/+page.svelte';
+
+   const routes = {
+     '/': Home,
+     '/mint': Mint,
+     '*': Home,
+   };
+
+   $: ({
+     colors: { background, window, text, highlight, highlightText },
+   } = $currentTheme);
++
++  onMount(() => {
++    // Initialize audio in the layout so it persists across routes
++    initializeAudio('./canttakemyeyesoffyou_1.mp3');
++  });
+ </script>
---
+++ src/lib/components/Home.svelte
@@ -0,0 +1,22 @@
+import schizodio_sq from '../../assets/images/schizodio_sq.jpg';
+import danceGif from '../../assets/images/dance-schizo.gif';
+import CopyButton from './CopyButton.svelte';
+import AudioPlayer from './AudioPlayer.svelte';
+import { togglePlayback } from '../stores/audio';
+import VolumeSlider from './VolumeSlider.svelte';
+import { link } from 'svelte-spa-router';
+import { onMount } from 'svelte';
+
+  hasSeenPopover = true;
+  showEnterPopover = false;
+  // Trigger audio play
+  togglePlayback();
+}
+</script>
+
+      </Window>
+      <Window title="💿schizo.mp3" width="full">
+        <AudioPlayer bind:this={audioPlayerRef} />
+      </Window>
+      <img src={schizodio_sq} alt="SCHIZODIO" width="auto" class="" />
+    </div>
