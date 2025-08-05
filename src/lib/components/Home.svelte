249c1cf9f861823032b33e06e76412f6e4c75ec1
bigjax <60930630+jaxnalia@users.noreply.github.com>
2025-08-05 15:10:29 +0100

    Move audio to layout with persistent playback

---
+++ src/lib/components/Home.svelte
@@ -0,0 +1,8 @@
+@@ .. @@
+       </Window>
+       <Window title="💿schizo.mp3" width="full">
+-        <AudioPlayer bind:this={audioPlayerRef} audioSrc="./canttakemyeyesoffyou_1.mp3" />
++        <AudioPlayer bind:this={audioPlayerRef} />
+       </Window>
+       <img src={schizodio_sq} alt="SCHIZODIO" width="auto" class="" />
+     </div>
