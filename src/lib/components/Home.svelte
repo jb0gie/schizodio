04e084b021bae8314d99cee464f47498c5ed0840
jaxnalia <jacksendall@gmail.com>
2025-08-05 15:22:28 +0100

    Revert "Move audio to layout with persistent playback"
    This reverts commit 249c1cf9f861823032b33e06e76412f6e4c75ec1.

--- src/lib/components/Home.svelte
+++
@@ -1,8 +0,0 @@
-@@ .. @@
-       </Window>
-       <Window title="💿schizo.mp3" width="full">
--        <AudioPlayer bind:this={audioPlayerRef} audioSrc="./canttakemyeyesoffyou_1.mp3" />
-+        <AudioPlayer bind:this={audioPlayerRef} />
-       </Window>
-       <img src={schizodio_sq} alt="SCHIZODIO" width="auto" class="" />
-     </div>
